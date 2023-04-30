449ecaad1371c714be33521cdf49bd560cda0bcc
brenda chepngetich <brendabre18@gmail.com>
2023-04-30 04:46:37 +0300

    linear regression for climate and conflicts

--- Interactions.R
+++ Interactions.R
@@ -48,3 +48,57 @@ hist(NDWS_sum)
 hist(NWLD_sum)
 hist(P95_sum)
 hist(TR_sum)
+
+#create a list of climate factors
+climate_lyrs <- list(CDD_sum,NDWS_sum,NT_sum,NWLD_sum,P95_sum,TR_sum)
+climate_lyrs
+
+#create variables for conflict data
+ACTOR1 <- Database$conflict$ACTOR1_RICHNESS_rfsi
+Actor2 <- Database$conflict$ACTOR2_RICHNESS_rfsi
+Events <- Database$conflict$EVENTS_rfsi
+Fatalities <- Database$conflict$FATALITIES_rfsi
+Subtype <- Database$conflict$SUBTYPE_RICHNESS_rfsi
+
+#create a list containing conflict layers
+conflict_layers <- list(ACTOR1,Actor2,Events,Fatalities,Subtype)
+conflict_layers
+
+#Regression
+#make linear model
+
+#loop through all conflict layers
+#rebuild conflict layers to have same extent as climate layers
+rebuilt_layers <- list() #create an empty list
+for (conf in conflict_layers)
+{
+  #obtain original name
+  name <- names(conf)
+  rebuilt_layer <- raster(vals=values(conf),ext=extent(CDD_Mean),crs=crs(CDD_Mean),
+                          nrows=dim(conf)[1],ncols=dim(conf)[2])
+  #resample to obtain similar resolution
+  resampled <- resample(rebuilt_layer,CDD_Mean)
+  stack()
+  names(resampled) <- name
+  rebuilt_layers<- c(rebuilt_layers,resampled)
+}
+
+#merge conflict list and climate list to have one list
+layers <- c(climate_lyrs,rebuilt_layers)
+layers
+#stack all layers
+stacked <- stack(layers)
+stacked
+
+#linear regression for climate versus conflict
+actor1_model <- lm(ACTOR1_RICHNESS_rfsi ~ CDD + NDWS + NT + NWLD + P95 + TR, data=as.data.frame(stacked))
+summary(actor1_model)
+actor2_model <- lm(ACTOR2_RICHNESS_rfsi ~ CDD + NDWS + NT + NWLD + P95 + TR, data=as.data.frame(stacked))
+summary(actor2_model)
+print("lll")
+EVENTS_rfsi <- lm(EVENTS_rfsi ~ CDD + NDWS + NT + NWLD + P95 + TR, data=as.data.frame(stacked))
+summary(EVENTS_rfsi)
+FATALITIES_rfsi <- lm(FATALITIES_rfsi ~ CDD + NDWS + NT + NWLD + P95 + TR, data=as.data.frame(stacked))
+summary(FATALITIES_rfsi)
+SUBTYPE_RICHNESS_rfsi <- lm(SUBTYPE_RICHNESS_rfsi ~ CDD + NDWS + NT + NWLD + P95 + TR, data=as.data.frame(stacked))
+summary(SUBTYPE_RICHNESS_rfsi)
