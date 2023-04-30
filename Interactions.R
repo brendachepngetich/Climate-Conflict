ab9342b637675774f55eac5d1230653890bb2d90
brenda chepngetich <brendabre18@gmail.com>
2023-04-30 04:42:08 +0300

    define sum for climate factors and assign new names

--- Interactions.R
+++ Interactions.R
@@ -2,6 +2,7 @@
 install.packages("ggplot2")
 install.packages("rlist")
 install.packages("raster")
+
 #load required packages
 library(raster)
 library(ggplot2)
@@ -9,11 +10,10 @@ library(rlist)
 
 #read data
 Database <- readRDS("~/Documents/interview/Database.rds")
-
 #view data object
 Database
 
-#Access individual raster
+#Access individual raster and calculate sum
 CDD <- Database$climate$CDD
 NDWS <- Database$climate$NDWS
 NT <- Database$climate$NTx35
@@ -21,16 +21,30 @@ NWLD <- Database$climate$NWLD
 P95 <- Database$climate$P95
 TR <- Database$climate$TR
 
-#calculate mean for each climate factor
-CDD_Mean <- calc(CDD, fun=mean)
-NDWS_Mean <- calc(NDWS, fun=mean)
-NT_Mean <- calc(NT, fun=mean)
-NWLD_Mean <- calc(NWLD, fun=mean)
-P95_Mean <- calc(P95, fun=mean)
-TR_Mean <- calc(TR, fun=mean)
+#calculate sum for each climate factor
+CDD_sum <- calc(CDD, fun=sum)
+NDWS_sum <- calc(NDWS, fun=sum)
+NT_sum <- calc(NT, fun=sum)
+NWLD_sum <- calc(NWLD, fun=sum)
+P95_sum <- calc(P95, fun=sum)
+TR_sum <- calc(TR, fun=sum)
 
-#plot consecutive dry days mean
+#assign names to sum layers
+names(CDD_sum) <- "CDD"
+names(NDWS_sum) <- "NDWS"
+names(NT_sum) <- "NT"
+names(NWLD_sum) <- "NWLD"
+names(P95_sum) <- "P95"
+names(TR_sum) <- "TR"
+
+
+#plot consecutive dry days sum
 plot(CDD_Mean, main="Mean Consecutive Dry Days", legend=TRUE, axes=TRUE)
 
 #check if data follows normal distribution
-hist(NDWS_Mean)
+hist(CDD_sum)
+hist(NT_sum)
+hist(NDWS_sum)
+hist(NWLD_sum)
+hist(P95_sum)
+hist(TR_sum)
